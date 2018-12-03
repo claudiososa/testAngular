1d2721befaa7e6e18bef4137b0bd5167703ced1e
Claudio Sosa <vicomser.claudio@gmail.com>
2018-12-03 08:37:43 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import { ResaltarDirective } from './directives/resaltar.directive';
+import { ContarClicksDirective } from './directives/contar-clicks.directive';
+import { Routes, RouterModule } from '@angular/router';
+import { DetalleComponent } from './detalle/detalle.component';
+
+const appRoutes: Routes = [
+  {path: '' , component: AppComponent},
+  {path: 'lugares', component: AppComponent},
+  {path: 'detalle', component: DetalleComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective,
+    ContarClicksDirective,
+    DetalleComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCEcwD83fDddsyyiC83-2_UAR-Eaq1chLQ'
+    }),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
